46acef8b3060ae76431845f76523877bd247b855
aluok <humbert.timede@gmail.com>
2016-08-14 22:43:01 +0200

    Implement the first DB service

---
+++ src/Garbanzo/DB/Services/MySQL.php
@@ -0,0 +1,35 @@
+<?php
+namespace Garbanzo\DB\Services;
+
+use Garbanzo\Kernel\Traits\ServiceCreation;
+
+class Mysql {
+    const OBJECT_RESULT = PDO::FETCH_OBJ;
+    const ARRAY_RESULT = PDO::FETCH_ASSOC;
+
+    use ServiceCreation;
+
+    protected $connexion;
+
+    public function connect($dbName, $host, $username, $password, $port = 3306) {
+        $this->connexion = new PDO('mysql:dbname=' . $dbName . ';host=' . $host . ';port=' . $port, $username, $password);
+    }
+
+    public function sendQuery($sql, $parameters) {
+        $this->connexion->prepare($sql)
+            ->execute($parameters);
+    }
+
+    public function get($sql, $parameters, $resultFormat = static::ARRAY_RESULT) {
+        return $this->connexion->prepare($sql)
+            ->execute($parameters)
+            ->fetch($resultFormat);
+    }
+
+    public function getAll($sql, $parameters, $resultFormat = static::ARRAY_RESULT) {
+        return $this->connexion->prepare($sql)
+            ->execute($parameters)
+            ->fetchAll($resultFormat);
+    }
+
+}
---
+++ src/Garbanzo/DBPlugin.php
@@ -0,0 +1,45 @@
+<?php
+namespace Garbanzo;
+
+use Garbanzo\Kernel\Interfaces\PluginInterface;
+use Garbanzo\Kernel\Interfaces\ContainerInterface;
+use Garbanzo\DB\Services\MySQL;
+
+class CMSPlugin implements PluginInterface{
+
+    protected $container;
+    protected $configuration;
+    protected $mainConfigFileName;
+    protected $namespace;
+
+    public function __construct($name, $mainConfigFileName) {
+        $this->namespace = $name;
+        $this->mainConfigFileName;
+    }
+
+    public function setContainer(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    public function getDefinedServices() {
+        return array(
+            'mysql' => MySQL::class,
+        );
+    }
+
+    public function loadDependencies() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function getServicesNamespace() {
+        return $this->namespace;
+    }
+
+    public function getConfiguration() {
+        return $this->configuration;
+    }
+}
